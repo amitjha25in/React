7067ca6094ca4439e32cb313b19d6654075f44bc
amit jha <amit.jha@candere.com>
2018-11-26 17:21:43 +0530

    React-Movie App
    React-Movie App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    data: []
+  };
+  componentDidMount() {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=ae2bf7ba6a38060f33525590abb745b2&language=en-US`
+    )
+      .then(res => res.json())
+      .then(json => this.setState({ data: json.results }));
+  }
+
   render() {
+    const path = "https://api.themoviedb.org/3/movie/";
+    console.log(this.state.data);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
+        <div className="container">
+          <div className="row">
+            {this.state.data.map(mv => (
+              <div className="col-6 col-sm-4 col-lg-3 mb-3" key={mv.id}>
+                <div className="card">
+                  <img
+                    className="card-img-top"
+                    src={`https://image.tmdb.org/t/p/w300/${mv.poster_path}`}
+                    alt="Movie Poster"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {mv.original_title.length < 10
+                        ? `${mv.original_title}`
+                        : `${mv.original_title.substring(0, 15)}...`}
+                    </h5>
+                    <p className="card-text">
+                      {mv.overview.length < 100
+                        ? `${mv.overview}`
+                        : `${mv.overview.substring(0, 100)}...`}
+                    </p>
+                    <a
+                      href={`https://www.themoviedb.org/movie/${mv.id}`}
+                      className="btn btn-primary"
+                      target="_blank"
+                    >
+                      View Details
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron text-center">
+        <h1>My First themoviedb React App</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
