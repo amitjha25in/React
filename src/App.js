479d79eb5aadd8ae4ac725f2d82ff5f866a3259e
amit jha <amit.jha@candere.com>
2019-01-30 17:54:37 +0530

    moviee

--- src/App.js
+++ src/App.js
@@ -55,7 +55,14 @@ class Home extends React.Component {
                           ? `${items.overview}`
                           : `${items.overview.substring(0, 35)}...`}
                       </p> */}
-                      <Link to={`/${items.id}`} className="btn btn-primary">
+
+                      <Link
+                        to={{
+                          pathname: `/${items.title.split(" ").join("-")}.html`,
+                          state: { title: items.title, id: items.id }
+                        }}
+                        className="btn btn-primary"
+                      >
                         See Profile
                       </Link>
                     </div>
@@ -70,18 +77,55 @@ class Home extends React.Component {
   }
 }
 class Movie extends React.Component {
+  state = {
+    movie: ""
+  };
+  componentDidMount() {
+    let newid = this.props.location.state.id;
+    fetch(
+      `https://api.themoviedb.org/3/movie/${newid}?api_key=ae2bf7ba6a38060f33525590abb745b2&language=en-US`
+    )
+      .then(res => res.json())
+      .then(json => this.setState({ movie: json }));
+  }
   render() {
-    console.log(this.props.location.pathname);
+    const {
+      title,
+      release_date,
+      poster_path,
+      overview,
+      homepage
+      // budget,
+      // status,
+      // revenue
+    } = this.state.movie;
     return (
       <div className="container">
         <div className="row mb-3">
           <div className="col-12">
-            <h3>You have Searched:{this.props.location.pathname}</h3>
+            <h3>You have Searched: {title}</h3>
           </div>
         </div>
         <div className="row">
-          <div className="col-12 col-sm-6 col-md-4 col-xl-3 mb-4">
-            Search Result
+          <div className="cl-6 mx-auto mb-4">
+            <div className="card">
+              <img
+                className="card-img-top"
+                src={`http://image.tmdb.org/t/p/w780${poster_path}`}
+                alt={title}
+              />
+              <div className="card-body">
+                <h4 className="card-title">{title}</h4>
+                <p>Release Date: {release_date}</p>
+                <p>{overview}</p>
+                <a href={homepage} className="btn btn-primary">
+                  Movie URL
+                </a>
+                <Link to="/" className="btn btn-primary">
+                  Back
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
