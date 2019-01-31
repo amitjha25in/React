90d77d933cd6df2a7db760bea297c76a87802e3b
amit jha <amit.jha@candere.com>
2019-01-31 16:25:46 +0530

    react form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-8 mx-auto">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="email">Email address:</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  autoComplete="true"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="pwd">Password:</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  autoComplete="true"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
