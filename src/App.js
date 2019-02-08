fd881f4811c059ac0a9a3472325e2b1324de2b57
amit jha <amit.jha@candere.com>
2019-02-08 16:02:43 +0530

    form implemented

--- src/App.js
+++ src/App.js
@@ -1,51 +1,115 @@
 import React, { Component } from "react";
-import Home from "./component/Home";
-import About from "./component/About";
-import Contact from "./component/Contact";
-import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import "./App.css";
 
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  contact: "",
+  errorFirstName: "",
+  errorEmail: "",
+  errorPassword: ""
+};
 class App extends Component {
-  state = {
-    name: "abcd"
+  state = initialState;
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  errorDetect = () => {
+    let errorFirstName = "";
+    let errorEmail = "";
+    let errorPassword = "";
+    if (this.state.firstName.length < 2) {
+      errorFirstName = "please enter your first name";
+    }
+    if (!this.state.email.includes("@")) {
+      errorEmail = "Please Enter corrent email";
+    }
+    if (this.state.password.length < 8) {
+      errorPassword = "please enter your password";
+    }
+    if (errorEmail || errorFirstName || errorPassword) {
+      this.setState({ errorFirstName, errorEmail, errorPassword });
+      return false;
+    }
+    return true;
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    const isValid = this.errorDetect();
+    if (isValid) {
+      console.log(this.state);
+      this.setState(initialState);
+    }
   };
   render() {
     return (
-      <BrowserRouter>
-        <div className="container my-5">
-          <nav>
-            <ul>
-              <li>
-                <NavLink to="/" activeClassName="selected">
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/about" activeClassName="selected">
-                  About
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to={{
-                    pathname: "/contact",
-                    state: { name: this.state.name }
-                  }}
-                  activeClassName="selected"
-                >
-                  Contact
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-          <Route>
-            <div className="row">
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/contact" component={Contact} />
-            </div>
-          </Route>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-sm-8 mx-auto">
+            <h3 className="text-center mb-5">Form</h3>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="firstName"
+                  value={this.state.firstName}
+                  onChange={this.handleChange}
+                />
+                <p>{this.state.errorFirstName}</p>
+              </div>
+              <div className="form-group">
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="lastName"
+                  value={this.state.lastName}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email address:</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+                <p>{this.state.errorEmail}</p>
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+                <p>{this.state.errorPassword}</p>
+              </div>
+              <div className="form-group">
+                <label htmlFor="contact">Contact:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="contact"
+                  value={this.state.contact}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
-      </BrowserRouter>
+      </div>
     );
   }
 }
