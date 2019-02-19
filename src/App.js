db13753558536095fe761a2672397f56a591b66d
amit jha <amit.jha@candere.com>
2019-02-19 18:30:43 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
+import data from "./Data.json";
 
 class App extends Component {
+  state = {
+    data: data
+  };
+  mouseEnter = () => {
+    const icons = document.querySelectorAll(".icon__");
+    for (var i = 0; i < icons.length; i++) {
+      icons[i].addEventListener("mouseover", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.mouseEnter();
+  }
   render() {
     return (
       <React.Fragment>
@@ -14,33 +32,32 @@ class App extends Component {
         </div>
         <div className="userWrapper">
           <div className="userImg">
-            <img
-              src="https://randomuser.me/api/portraits/women/68.jpg"
-              alt="user"
-            />
+            <img src={this.state.data[0].picture.thumbnail} alt="user" />
           </div>
           <div className="userInfo">
             <p className="user_title">Hi, My name is</p>
-            <p className="user_value">gail fernandez</p>
+            <p className="user_value">
+              {this.state.data[0].name.first + this.state.data[0].name.last}
+            </p>
           </div>
           <div className="social">
-            <span>
-              <i class="user" />
+            <span data-value={this.state.data[0].email}>
+              <i className="icon__ user active" />
             </span>
-            <span>
-              <i class="envelope" />
+            <span data-value={this.state.data[0].dob.date}>
+              <i className="icon__ envelope" />
             </span>
-            <span>
-              <i class="calendar" />
+            <span data-value={this.state.data[0].email}>
+              <i className="icon__ calendar" />
             </span>
-            <span>
-              <i class="map-marker" />
+            <span data-value={this.state.data[0].location.city}>
+              <i className="icon__ map-marker" />
             </span>
-            <span>
-              <i class="phone" />
+            <span data-value={this.state.data[0].phone}>
+              <i className="icon__ phone" />
             </span>
-            <span>
-              <i class="unlock-alt" />
+            <span data-value={this.state.data[0].login.password}>
+              <i className="icon__ unlock-alt" />
             </span>
           </div>
         </div>
