590691afa457a1721dc067e961884c69ca53d6a6
amit jha <amit.jha@candere.com>
2019-03-15 12:58:01 +0530

    fetched locally data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    data: []
+  };
+  componentDidMount() {
+    axios.get(`http://localhost/testing/json/json.php`).then(res => {
+      const data = res.data;
+      this.setState({ data });
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="row items">
+          {this.state.data.map(item => {
+            return (
+              <div className="flex__ text-center" key={item.id}>
+                <span>{item.country_name}</span>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
