c64fe72d347e56c6443b33407f8a2ef75642ce6a
amit jha <amit.jha@candere.com>
2019-03-16 12:53:14 +0530

    update

--- src/php/json.php
+++ src/php/json.php
@@ -2,7 +2,7 @@
     require 'core.php';
 //ORDER BY id DESC
     $connect = mysqli_connect("localhost", "root", "", "testing");  
-    $sql = "SELECT * FROM apps_countries  ";  
+    $sql = "SELECT * FROM apps_countries ORDER BY ID DESC ";  
     $result = mysqli_query($connect, $sql);  
     $json_array = array();  
     while($row = mysqli_fetch_assoc($result))  
--- src/php/post.php
+++ src/php/post.php
@@ -11,11 +11,13 @@
         $country_name = mysqli_real_escape_string($connect, $post["posts"]["country_name"]); 
          
         $query = "INSERT INTO apps_countries(country_code, country_name) VALUES('".$country_code."', '".$country_name."')";  
+        
         if(mysqli_query($connect, $query))  
         {  
-            echo '<p>You have entered</p>';  
-            echo '<p>Name:'.$country_code.'</p>';  
-            echo '<p>Message : '.$country_name.'</p>';  
+            $arr=array();
+            $arr['country_code']=$country_code;
+            $arr['country_name']=$country_name;
+            echo json_encode($arr);   
         }  
     }  
 ?>  
