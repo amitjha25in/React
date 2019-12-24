0ac11883e316b976749f89c7320c3a77b06a7f2d
amit jha <amit.jha@candere.com>
2019-12-24 15:30:25 +0530

    user list completed

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,21 +1,52 @@
 import React, { Component } from "react";
-import { User } from "../Data/Data";
+import { User } from "../Data/User";
+import Single_User from "./SingleUser";
 
 class People extends Component {
   state = {
-    peopleName: User
+    userData: User
   };
-
+  handleClick = (id) => {
+    const { userData } = this.state;
+    const removedId = userData.filter(uid => uid.id !== id);
+    this.setState({
+      userData: removedId
+    })
+  }
+  //this is regular method
+  // render() {
+  //   const { userData } = this.state;
+  //   return (
+  //     <div className="container">
+  //       <div className="row">
+  //         {userData.map(user_info => {
+  //           return <Single_User key={user_info.id} user={user_info} removeUser={this.handleClick} />;
+  //         })}
+  //       </div>
+  //     </div>
+  //   );
+  // }
+  //is is conditional method
   render() {
-    console.log(this.state.peopleName);
-    const { peopleName } = this.state;
-
+    const { userData } = this.state;
+    const userList = userData.map(user_info => {
+      return <Single_User key={user_info.id} user={user_info} removeUser={this.handleClick} />;
+    });
+    const noData = () => {
+      return (
+        <div className="col-sm-12 mt-5">
+          <div className="alert alert-danger">
+            <strong>Danger!</strong> You have Deleted all the data.
+          </div>
+        </div>
+      )
+    }
     return (
-      <>
-        {peopleName.map(item => {
-          return <span>{item.id}</span>;
-        })}
-      </>
+      <div className="container">
+        <div className="row">
+          {userData.length > 0 ? userList : noData()}
+        </div>
+      </div>
     );
   }
 }
---
+++ src/Components/SingleUser.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Single_User extends Component {
+
+    render() {
+        const { id, name, phone, email, company, } = this.props.user;
+        const { removeUser } = this.props;
+        return (
+            <div className="col-sm-4 mb-4">
+                <div className="card">
+                    <img className="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image" />
+                    <div className="card-body">
+                        <h4 className="card-title">Name: {name}</h4>
+                        <p className="card-text">Email: {email}</p>
+                        <p className="card-text">Company: {company.name}</p>
+                        <p className="card-text">Phone: {phone}</p>
+                        <button className="btn btn-primary" onClick={() => removeUser(id)}>Remove</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Single_User;
